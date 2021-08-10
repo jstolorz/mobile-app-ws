3d999dff97e359a52f09a75aee889c5c594ef921
janusz <jstolorz@gmail.com>
2021-08-10 18:46:19 +0200

    Initial commit

---
+++ src/main/java/com/bluesoft/app/ws/ui/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.bluesoft.app.ws.ui.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+class UserController {
+
+}
