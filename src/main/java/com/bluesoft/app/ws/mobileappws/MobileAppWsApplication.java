da606df6b6abe3f4094885954608a70c987ed1e0
janusz <jstolorz@gmail.com>
2021-08-10 19:12:47 +0200

    adding UserController with skeleton of GET,POST,PUT,DELETE method

--- src/main/java/com/bluesoft/app/ws/mobileappws/MobileAppWsApplication.java
+++ src/main/java/com/bluesoft/app/ws/mobileappws/MobileAppWsApplication.java
@@ -2,8 +2,10 @@ package com.bluesoft.app.ws.mobileappws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.bluesoft.app.ws")
 public class MobileAppWsApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/bluesoft/app/ws/ui/controller/UserController.java
+++ src/main/java/com/bluesoft/app/ws/ui/controller/UserController.java
@@ -1,10 +1,35 @@
 package com.bluesoft.app.ws.ui.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
-class UserController {
+public class UserController {
+
+    @GetMapping
+    public String getUser(){
+        return "get user was called";
+    }
+
+    @GetMapping("/{id}")
+    public String getUserById(@PathVariable Long id){
+        return "get user by id: " + id + " was called";
+    }
+
+    @PostMapping
+    public String createUser(){
+        return "create user was called";
+    }
+
+    @PutMapping
+    public String updateUser(){
+        return "update user was called";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable Long id){
+        return "delete user with id: " + id + " was called";
+    }
 
 }
